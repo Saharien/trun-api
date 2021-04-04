db3f91de95b4d15cbeaf3fa7844e54e72b6c191b
Saharien <mika@mkarl.de>
2021-04-04 23:39:00 +0200

    OPTIONS noch erlaubt in den Headern

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var port = process.env.PORT || 8080;
 // Um Probleme mit Chrome CORS Policy zu vermeiden
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
     return next();
 });
 
